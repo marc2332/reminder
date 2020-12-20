2939e1e03a4e902e409936571f38562affd0d3d9
Marc <mespinsanz@gmail.com>
2020-12-20 12:11:00 +0100

    :tada: First commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{thread, time};
+use notifica::{{notify}};
+
+fn main(){
+	execute(1800000, false) // Every 30 minutes
+}
+
+fn execute(delay: u64, log: bool){
+	let ten_millis = time::Duration::from_millis(delay);
+	loop {
+		thread::sleep(ten_millis);
+		let result = notify("Reminder", "Look away from the screen 😵");
+		if log == true{
+			match result {
+				Ok(_) => println!("Sent notification"),
+				Err(_) => println!("Error")
+			}
+		}
+	}
+}
+
+#[test]
+fn test(){
+	execute(2000, true)
+}
